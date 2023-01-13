0c030ef5cbb0496590bbe0ec9763584bb99e25d4
trottling <truewasted@yandex.ru>
2023-01-13 10:42:18 -0500

    add build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+export ARCH=arm64
+export SUBARCH=arm64
+export CC=clang
+export CLANG_TRIPLE=clang-
+export CROSS_COMPILE=aarch64-linux-gnu-
+
+
+make O=out -j12
+
+
+
+
+
+#make menuconfig O=out
+
+#make evo_defconfig -j5
