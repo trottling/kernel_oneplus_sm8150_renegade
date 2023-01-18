7176bd2e75b15c1d60a782d50adadd4859a0276d
trottling <truewasted@yandex.ru>
2023-01-18 13:07:25 -0500

    Update build script

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 export ARCH=arm64
 export SUBARCH=arm64
+export HEADER_ARCH=arm64
+#export DTC_EXT=dtc
 export CC=clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=aarch64-linux-gnu-
 
+PATH="/home/kali/dev/compiler/clang-r450784d/bin/:/home/kali/dev/compiler/aarch64-android-t-qpr2-beta-2_r0.2/bin/:/home/kali/dev/compiler/arm-android-t-qpr2-beta-2_r0.2/${PATH}"
 
-make O=out -j12 evo_renegade_defconfig
-
-make O=out -j12
-
+make O=out -j($(nproc --all)+1) evo_renegade_defconfig ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-
 
 
-#make menuconfig O=out
+make O=out -j($(nproc --all)+1) ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-
 
-#make evo_defconfig -j5
+
+
